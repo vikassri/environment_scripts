cc05c556a48c3dd4ffb1650a3b2207556e8a6686
vikassri <er.vikassri@gmail.com>
2021-05-24 10:31:28 +0530

    updated

--- env/variables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export base_path="/Users/vssrivastava/Work/Install"
-export PATH="$PATH:$base_path/executables"
-export PATH="$PATH:$base_path/packer/bin"
--- env/ycloud.sh
+++
@@ -1,6 +0,0 @@
-alias master01='ssh root@master01'
-alias master02='ssh root@master02'
-alias slave01='ssh root@slave01'
-alias slave02='ssh root@slave02'
-alias slave03='ssh root@slave03'
-
